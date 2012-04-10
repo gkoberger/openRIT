994a4d655bc4d31077e4600363c70e61be86c5b7
Gregory Koberger <gkoberger@gmail.com>
2012-04-09 23:27:49 -0700

    Round one

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.links a').fancybox({
+      maxWidth	: 800,
+      maxHeight	: 600,
+      fitToView	: false,
+      width		: 400,
+      height		: 280,
+      autoSize	: false,
+      closeClick	: false,
+      openEffect	: 'none',
+      closeEffect	: 'none'
+  });
+});
