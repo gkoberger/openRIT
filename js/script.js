9bb8fe70ac16ddb331c3b2721ecf813ca54a78cd
Gregory Koberger <gkoberger@gmail.com>
2012-04-14 00:31:35 -0700

    Add position applications

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
-  $('.links a').fancybox({
-      maxWidth	: 800,
-      maxHeight	: 600,
-      fitToView	: false,
-      width		: 400,
-      height		: 280,
-      autoSize	: false,
-      closeClick	: false,
-      openEffect	: 'none',
-      closeEffect	: 'none'
-  });
+    $('#open').fancybox({
+        maxWidth	: 800,
+        maxHeight	: 600,
+        fitToView	: false,
+        width		: 500,
+        height		: 400,
+        autoSize	: false,
+        closeClick	: false,
+        openEffect	: 'none',
+        closeEffect	: 'none'
+    });
+
+    $('.links a, .signup').fancybox({
+        maxWidth	: 800,
+        maxHeight	: 600,
+        fitToView	: false,
+        width		: 400,
+        height		: 280,
+        autoSize	: false,
+        closeClick	: false,
+        openEffect	: 'none',
+        closeEffect	: 'none'
+    });
+    $('#open-div').submit(function(e) {
+        e.preventDefault();
+        $(this).find('button').attr('disabled', true).text('Submitting...');
+        var info = $(this).serializeArray();
+        console.log(info);
+        $.post('submit.php', info, function() {
+            alert('Done!');
+        });
+    });
 });
